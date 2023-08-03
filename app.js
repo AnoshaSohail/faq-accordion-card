87c6bdd66c9b14729eb7e8def7c71d977a46b963
Anosha <anoshasohail676@gmail.com>
2023-08-03 17:51:08 +0500

    anosha

--- app.js
+++ app.js
@@ -1,42 +1,30 @@
-const answerDiv = document.querySelectorAll('.accordian__colapse');
-const question = document.querySelectorAll('.question');
+const answerDiv = document.querySelectorAll(".accordian__colapse");
+const question = document.querySelectorAll(".question");
 let isOpen = null;
 let previousQuestion = null;
 
 function openItem(event) {
-
     /*  if(previousQuestion !== event.target){
-       
-            
-         
+          if(!(question.classList.contains('open'))) {
+           answer.classList.remove('open');
+           answer.classList.remove('collapse')
+          }
      } */
-     
+
     if (event) {
-        
         isOpen = true;
-        event.target.classList.toggle('open');
+        event.target.classList.toggle("open");
         const parent = event.target.parentNode;
         const answer = parent.nextElementSibling;
-
-        if (event.target.classList.contains('open')) {
-
-            answer.classList.add('collapse');
-
-
-
+        if (event.target.classList.contains("open")) {
+            answer.classList.add("collapse");
         } else {
-
-            answer.classList.remove('collapse');
-
+            answer.classList.remove("collapse");
         }
-        event.target.classList.toggle('question__hover');
-        
+        event.target.classList.toggle("question__hover");
         previousQuestion = event.target;
     }
-   
-
 }
-question.forEach(questions => {
-    questions.addEventListener('click', openItem);
-    
-})
+question.forEach((questions) => {
+    questions.addEventListener("click", openItem);
+});
