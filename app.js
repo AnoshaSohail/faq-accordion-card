a7c19aeb7b78d31a1dbb660136669d9689eac908
Anosha <anoshasohail676@gmail.com>
2023-08-03 14:53:48 +0500

    ansha

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const answerDiv = document.querySelectorAll('.accordian__colapse');
 const question = document.querySelectorAll('.question');
 let isOpen = null;
 let previousQuestion = null;
+
 function openItem(event) {
+
     /*  if(previousQuestion !== event.target){
        
             
          
      } */
+     
     if (event) {
+        
         isOpen = true;
         event.target.classList.toggle('open');
         const parent = event.target.parentNode;
@@ -25,11 +29,14 @@ function openItem(event) {
             answer.classList.remove('collapse');
 
         }
-        event.target.classList.remove('question__hover');
+        event.target.classList.toggle('question__hover');
+        
         previousQuestion = event.target;
     }
+   
 
 }
 question.forEach(questions => {
     questions.addEventListener('click', openItem);
+    
 })
